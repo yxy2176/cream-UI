1e0773d78772eb933b74efdacc42088a4ed17970
yxy2176 <344561707@qq.com>
2025-02-25 21:24:56 +0800

    feat:修改Collapse中transition的过渡时间和初始化Icon组件的文件目录

---
+++ src/components/Icon/icon.vue
@@ -0,0 +1,10 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div></div>
+</template>
+<script setup lang='ts'>
+
+</script>
+<style scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import '../src/styles/index.css'
 import { library } from '@fortawesome/fontawesome-svg-core' // Font Awesome 的核心库，用于管理图标集合
 import { fas } from '@fortawesome/free-solid-svg-icons' // 从 @fortawesome/free-solid-svg-icons 中导入的所有 ​Solid 风格图标
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome' //Vue 组件，用于在模板中渲染图标
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome' //Vue 组件，用于在模板中渲染图标
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// createApp(App).mount('#app')
+createApp(App).mount('#app')
 
 library.add(fas) //将图标添加到库中
-createApp(App)
-  .component('font-awesome-icon', FontAwesomeIcon) // 将 FontAwesomeIcon 组件全局注册为 font-awesome-icon，以便在项目的任何地方使用
-  .mount('#app')
+// 将 FontAwesomeIcon 组件全局注册为 font-awesome-icon，以便在项目的任何地方使用
+// createApp(App)
+// .component('font-awesome-icon', FontAwesomeIcon)
+// .mount('#app')
