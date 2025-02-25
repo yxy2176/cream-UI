464d201abb48852b82bbcbe180bf52a3868d9a9c
yxy2176 <344561707@qq.com>
2025-02-25 21:08:44 +0800

    feat:Collapse组件的样式和动画过渡效果

--- src/components/Collapse/CollapseItem.vue
+++ src/components/Collapse/CollapseItem.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="xy-collapse-item" :class="{ 'is-disabled': disabled }">
-    <div class="xy-collapse-item__header" :id="`item-header-${name}`" @click="handleClick">
+    <div
+      class="xy-collapse-item__header"
+      :class="{
+        'is-disabled': disabled,
+        'is-active': isActive,
+      }"
+      :id="`item-header-${name}`"
+      @click="handleClick"
+    >
       <slot name="title">{{ title }}</slot>
     </div>
-    <div class="xy-collapse-item__content" :id="`item-content-${name}`" v-show="isActive">
-      <slot />
-    </div>
+
+    <Transition name="slide" v-on="transitionEvents">
+      <div class="xy-collapse-item__wrapper" v-show="isActive">
+        <div class="xy-collapse-item__content" :id="`item-content-${name}`" v-show="isActive">
+          <slot />
+        </div>
+      </div>
+    </Transition>
   </div>
 </template>
 <script setup lang="ts">
@@ -24,6 +37,31 @@ const handleClick = () => {
     return
   }
   collapseContext?.handleItemClick(props.name)
+}
+
+const transitionEvents: Record<string, (el: HTMLElement) => void> = {
+  beforeEnter(el) {
+    el.style.height = '0px'
+    el.style.overflow = 'hidden'
+  },
+  enter(el) {
+    el.style.height = `${el.scrollHeight}px`
+  },
+  afterEnter(el) {
+    el.style.height = ''
+    el.style.overflow = ''
+  },
+  beforeLeave(el) {
+    el.style.height = `${el.scrollHeight}px`
+    el.style.overflow = 'hidden'
+  },
+  leave(el) {
+    el.style.height = '0px'
+  },
+  afterLeave(el) {
+    el.style.height = ''
+    el.style.overflow = ''
+  },
 }
 </script>
 <style scoped>
